b48b0f220bb3c53e165f47e98c82d1dd7ab87a92
Mahadev Maneesh <maha040963@sisharjah.com>
2024-05-25 10:35:36 +0400

    login back end without sha256

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
 import eel 
+import back 
+eel.init('public')
+
+
+
+@eel.expose
+
+def getdata(u,p):
+   l =  back.confirmlogin(p)
+   if l == u :
+      return True
+   else:
+      return False
+@eel.expose
+def regdata(u,p):
+   if u != '' and p !='' :
+      l = back.register(u,p)
+      return l
+   else:
+      return 'Blank user and pass'
+   
+   
+
+
+eel.start('login.html' ,size=(1114,654) )
