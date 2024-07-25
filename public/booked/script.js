a1f085be9f3bbd9c2bb6c87f38d0d55b6e5eda21
Mahadev Maneesh <maha040963@sisharjah.com>
2024-07-25 23:01:19 +0400

    ticket

--- public/booked/script.js
+++ public/booked/script.js
@@ -8,56 +8,61 @@ async function ticketLoad() {
   ticketsdata.forEach((ticket) => {
     pash = "";
     ticket.passengerdetails.forEach((passenger) => {
-      pshtml = ` <div class="passenger">
-      <h1 class="passenger-head">Passport details</h1>
-      <div class="passenger-cont">
-        <div class="login-inf">
-          <span>Title</span>
-          <span class="login-text" >
-          ${passenger.title}
-          </span>
-        </div>
-        <div class="login-inf">
-          <span>Name</span>
-          <span class="login-text" id="username" type="text" name="" id="" >
-          ${passenger.firstName}</span>
-        </div>
-       
+      pshtml = `<link rel="stylesheet" href="bookings/style.css">
+<div class="passenger">
+    <div class="passenger-cont">
+      <div class="login-inf">
+        <span>Title</span>
+        <span class="login-text" >
+        ${passenger.title}
+        </span>
       </div>
-      <div class="passenger-cont">
-        <div class="login-inf">
+      <div class="login-inf">
+        <span>Name</span>
+        <span class="login-text" id="username" type="text" name="" id="" >
+        ${passenger.firstName}</span>
+      </div>
+     
+    </div>
+    <div class="passenger-cont">
+      <div class="login-inf">
 <span>Age group</span>
 <span class="login-text"> ${passenger.ageGroup} </span>
-</div>
-</div>
-        <div class="login-inf">
-          <span>Nationality</span>
-          <span class="login-text" >
-            ${passenger.national}
-          </span>
-        </div>
+</div><div class="login-inf">
+        <span>Nationality</span>
+        <span class="login-text" >
+          ${passenger.national}
+        </span>
       </div>
+</div>
 <div class="passenger-cont">
-<div class="login-inf">
-<span>Food Choice</span>
-<span class="login-text btn" >${passenger.food[0]}</span>
-</div>
-<div class="login-inf">
-<span>Seat</span>
-<span class="login-text ste" >${passenger.seat}</span>
-</div>
-<div class="login-inf">
-<span>Baggage</span>
-<span class="login-text" >
-${passenger.bgg}
-</span>
-</div>
-</div>
-         
-    </div>`;
+    <div class="login-inf">
+    <span>Food Choice</span>
+    <span class="login-text btn" >${passenger.food[0]}</span>
+    </div>
+    <div class="login-inf">
+    <span>Seat</span>
+    <span class="login-text ste" >${passenger.seat}</span>
+    </div>
+    <div class="login-inf">
+    <span>Baggage</span>
+    <span class="login-text" >
+    ${passenger.bgg}
+    </span>
+    </div>
+    </div>
+           <h2 class="price-cont indi">Price<br> <span class="price" id="totprice">${
+             "$" + passenger.indi_price
+           }</span></h2>
+      </div>
+    
+    
+
+
+  `;
       pash += pshtml;
     });
-    htm = `<div class="ticket-passenger flight">
+    htm = `<div class="ticket-passenger flight flight-marg ">
           <div class="flight tto">
             <div class="flight-dest-cont">
               <span class="flight-dest">${ticket.from}</span>
@@ -71,7 +76,9 @@ ${passenger.bgg}
                   <span class="flight-date flight-time">${ticket.time}</span>
 
                   <span class="flight-time">$${ticket.price}</span>
-                  <span class="flight-date flight-time">${ticket.duration}</span>
+                  <span class="flight-date flight-time">${
+                    ticket.duration
+                  }</span>
                 </div>
               </div>
             </div>
@@ -82,11 +89,12 @@ ${passenger.bgg}
 
               <span class="flight-dest">${ticket.to}</span>
             </div>
+            
           </div>
           ${pash}
-        
-            <h2 style="text-align: right; width: 100%">Total Price : $<span id="totprice">${ticket.totalprice}</span></h2>
-            <div id="passengers"></div>
+        <h2 class="price-cont">Total Price<br> <span class="price" id="totprice">${
+          "$" + ticket.totalprice
+        }</span></h2>
           </div>`;
 
     document.querySelector("#startkaro").insertAdjacentHTML("afterend", htm);
